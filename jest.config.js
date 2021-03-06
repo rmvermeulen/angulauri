f3041f45a82f515caff8d6105a72808f8527dceb
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-06 19:10:00 +0100

    tauri-service: fix lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
   },
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["jest-preset-angular/setup-jest"],
 };
--- src/app/tauri.service.ts
+++ src/app/tauri.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core'
-import { promisified } from 'tauri/api/tauri'
-
+import { Injectable } from '@angular/core';
+import { snakeCase } from 'snake-case';
+import { promisified } from 'tauri/api/tauri';
 export type Cmd =
   | { cmd: 'getCwd' }
-  | { cmd: 'getItems'; id: string; page: number; page_size: number }
-  | { cmd: 'createResource'; items: string[] }
+  | { cmd: 'getItems'; id: string; page: number; pageSize: number }
+  | { cmd: 'createResource'; items: string[] };
 
 export interface GetItemsResponse<T> {
-  items: T[]
-  hasPrev: boolean
-  hasNext: boolean
+  items: T[];
+  hasPrev: boolean;
+  hasNext: boolean;
 }
+
+const rustifyObject: (o: Record<string, unknown>) => Record<string, unknown> = (
+  o
+) =>
+  Object.fromEntries(
+    Object.entries(o).map(([k, v]: [string, unknown]) => [snakeCase(k), v])
+  );
 
 @Injectable({
   providedIn: 'root',
@@ -19,21 +26,21 @@ export class TauriService {
   constructor() {}
 
   async getCwd(): Promise<string> {
-    return this.cmd<string>({ cmd: 'getCwd' })
+    return this.cmd<string>({ cmd: 'getCwd' });
   }
   async getItems<T = string>(
     id: string,
     page = 0,
-    page_size = 10
+    pageSize = 10
   ): Promise<GetItemsResponse<T>> {
-    return this.cmd({ cmd: 'getItems', id, page, page_size })
+    return this.cmd({ cmd: 'getItems', id, page, pageSize });
   }
 
   async createResource(items: string[]): Promise<string> {
-    return this.cmd<string>({ cmd: 'createResource', items })
+    return this.cmd<string>({ cmd: 'createResource', items });
   }
 
   private async cmd<R>(cmd: Cmd): Promise<R> {
-    return promisified(cmd)
+    return promisified(rustifyObject(cmd));
   }
 }
