0fbad1f4ca445ae2e721dc125293b52d866d1205
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-06 15:32:41 +0100

    Do the whole thing with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,61 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, from, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { TauriService } from './tauri.service';
-
 @Component({
   selector: 'app-root',
-  styleUrls: ['./app.component.scss'],
+  styles: [
+    `
+      div * {
+        display: inline-block;
+      }
+    `,
+  ],
   template: `
     <span>{{ title }} app is running!</span>
-    <p>cwd: {{ cwd }}</p>
-    <p>items: {{ items }}</p>
+    <p>cwd: {{ cwd$ | async }}</p>
+    <p>items: {{ items$ | async }}</p>
+    <div>
+      <button (click)="decPage()">&lt;</button>
+      <p>{{ page$ | async }}</p>
+      <button (click)="incPage()">&gt;</button>
+    </div>
+    <div>
+      <button (click)="decPageSize()">&lt;</button>
+      <p>{{ pageSize$ | async }}</p>
+      <button (click)="incPageSize()">&gt;</button>
+    </div>
     <router-outlet></router-outlet>
-    <button (click)="clearCwd()">Clear cwd</button>
-    <button (click)="getCwd()">Fetch cwd</button>
   `,
 })
 export class AppComponent {
   title: string = 'Angulauri';
-  cwd: string = '';
-  items: number[] = [];
+  cwd$: Observable<string>;
+  items$: Observable<string[]>;
+  page$ = new BehaviorSubject(0);
+  pageSize$ = new BehaviorSubject(10);
   constructor(private readonly tauri: TauriService) {
-    this.getCwd();
-    this.getItems();
+    this.cwd$ = from(this.tauri.getCwd());
+    this.items$ = combineLatest([this.page$, this.pageSize$]).pipe(
+      switchMap(([page, pageSize]: [number, number]) =>
+        from(this.tauri.getItems('id', page, pageSize))
+      )
+    );
   }
 
-  async getCwd() {
-    this.cwd = await this.tauri.getCwd();
+  incPage() {
+    this.page$.next(this.page$.value + 1);
   }
-  async getItems() {
-    this.items = await this.tauri.getItems('asd');
+
+  decPage() {
+    this.page$.next(Math.max(0, this.page$.value - 1));
   }
-  setCwd(cwd: string) {
-    this.cwd = cwd;
+
+  incPageSize() {
+    this.pageSize$.next(this.pageSize$.value + 1);
   }
-  clearCwd() {
-    this.setCwd('');
+
+  decPageSize() {
+    this.pageSize$.next(Math.max(1, this.pageSize$.value - 1));
   }
 }
