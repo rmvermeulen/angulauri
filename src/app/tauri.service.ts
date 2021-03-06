b643353a33034f0b4c27617c8310c0c5de5dbde8
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-06 19:12:20 +0100

    auto-fix linter errors

--- src/app/tauri.service.ts
+++ src/app/tauri.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { snakeCase } from 'snake-case';
-import { promisified } from 'tauri/api/tauri';
+import { Injectable } from '@angular/core'
+import { snakeCase } from 'snake-case'
+import { promisified } from 'tauri/api/tauri'
 export type Cmd =
   | { cmd: 'getCwd' }
   | { cmd: 'getItems'; id: string; page: number; pageSize: number }
-  | { cmd: 'createResource'; items: string[] };
+  | { cmd: 'createResource'; items: string[] }
 
 export interface GetItemsResponse<T> {
-  items: T[];
-  hasPrev: boolean;
-  hasNext: boolean;
+  items: T[]
+  hasPrev: boolean
+  hasNext: boolean
 }
 
 const rustifyObject: (o: Record<string, unknown>) => Record<string, unknown> = (
@@ -17,7 +17,7 @@ const rustifyObject: (o: Record<string, unknown>) => Record<string, unknown> = (
 ) =>
   Object.fromEntries(
     Object.entries(o).map(([k, v]: [string, unknown]) => [snakeCase(k), v])
-  );
+  )
 
 @Injectable({
   providedIn: 'root',
@@ -26,21 +26,21 @@ export class TauriService {
   constructor() {}
 
   async getCwd(): Promise<string> {
-    return this.cmd<string>({ cmd: 'getCwd' });
+    return this.cmd<string>({ cmd: 'getCwd' })
   }
   async getItems<T = string>(
     id: string,
     page = 0,
     pageSize = 10
   ): Promise<GetItemsResponse<T>> {
-    return this.cmd({ cmd: 'getItems', id, page, pageSize });
+    return this.cmd({ cmd: 'getItems', id, page, pageSize })
   }
 
   async createResource(items: string[]): Promise<string> {
-    return this.cmd<string>({ cmd: 'createResource', items });
+    return this.cmd<string>({ cmd: 'createResource', items })
   }
 
   private async cmd<R>(cmd: Cmd): Promise<R> {
-    return promisified(rustifyObject(cmd));
+    return promisified(rustifyObject(cmd))
   }
 }
