f763e50c9a939e5f4dceee8f78caa94d694cfdb2
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-06 18:21:09 +0100

    app: fix lint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core'
-import { BehaviorSubject, combineLatest, from, Observable } from 'rxjs'
-import { map, pluck, switchMap } from 'rxjs/operators'
-import { TauriService, GetItemsResponse } from './tauri.service'
+import { Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, from, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { GetItemsResponse, TauriService } from './tauri.service';
 
 @Component({
   selector: 'app-root',
@@ -19,11 +19,11 @@ import { TauriService, GetItemsResponse } from './tauri.service'
     <p>has-prev: {{ hasPrevPage$ | async }}</p>
     <p>has-next: {{ hasNextPage$ | async }}</p>
     <div>
-      <button (click)="decPage()" [disabled]="!(hasPrevPage$ | async)">
+      <button (click)="decPage()" [disabled]="(hasPrevPage$ | async) === false">
         &lt;
       </button>
       <p>{{ page$ | async }}</p>
-      <button (click)="incPage()" [disabled]="!(hasNextPage$ | async)">
+      <button (click)="incPage()" [disabled]="(hasNextPage$ | async) === false">
         &gt;
       </button>
     </div>
@@ -36,39 +36,39 @@ import { TauriService, GetItemsResponse } from './tauri.service'
   `,
 })
 export class AppComponent {
-  title = 'Angulauri'
-  page$ = new BehaviorSubject(0)
-  pageSize$ = new BehaviorSubject(10)
-  cwd$: Observable<string>
-  response$: Observable<GetItemsResponse<string>>
-  items$: Observable<string[]>
-  hasNextPage$: Observable<Boolean>
-  hasPrevPage$: Observable<Boolean>
+  title = 'Angulauri';
+  page$ = new BehaviorSubject(0);
+  pageSize$ = new BehaviorSubject(10);
+  cwd$: Observable<string>;
+  response$: Observable<GetItemsResponse<string>>;
+  items$: Observable<string[]>;
+  hasNextPage$: Observable<boolean>;
+  hasPrevPage$: Observable<boolean>;
   constructor(private readonly tauri: TauriService) {
-    this.cwd$ = from(this.tauri.getCwd())
+    this.cwd$ = from(this.tauri.getCwd());
     this.response$ = combineLatest([this.page$, this.pageSize$]).pipe(
       switchMap(([page, pageSize]: [number, number]) =>
         from(this.tauri.getItems<string>('id', page, pageSize))
       )
-    )
-    this.items$ = this.response$.pipe(map(({ items }) => items))
-    this.hasNextPage$ = this.response$.pipe(map(({ hasNext }) => hasNext))
-    this.hasPrevPage$ = this.response$.pipe(map(({ hasPrev }) => hasPrev))
+    );
+    this.items$ = this.response$.pipe(map(({ items }) => items));
+    this.hasNextPage$ = this.response$.pipe(map(({ hasNext }) => hasNext));
+    this.hasPrevPage$ = this.response$.pipe(map(({ hasPrev }) => hasPrev));
   }
 
   incPage() {
-    this.page$.next(this.page$.value + 1)
+    this.page$.next(this.page$.value + 1);
   }
 
   decPage() {
-    this.page$.next(Math.max(0, this.page$.value - 1))
+    this.page$.next(Math.max(0, this.page$.value - 1));
   }
 
   incPageSize() {
-    this.pageSize$.next(this.pageSize$.value + 1)
+    this.pageSize$.next(this.pageSize$.value + 1);
   }
 
   decPageSize() {
-    this.pageSize$.next(Math.max(1, this.pageSize$.value - 1))
+    this.pageSize$.next(Math.max(1, this.pageSize$.value - 1));
   }
 }
