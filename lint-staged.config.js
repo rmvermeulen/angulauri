10b7ecdf00cee3038b064e790ad182b1e04c36cb
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-07 11:10:13 +0100

    lint-staged: update src-tauri test cmds to work run as 'lint-staged -x'

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,23 @@
+// @ts-check
+const { pipe, test, partition } = require("ramda");
+
 module.exports = {
   "src/**/*.{js,ts}": [
     // (files) => `ng lint --fix --lint-file-patterns=${files.join(",")}`,
     (_) => "ng lint --fix",
     "jest --findRelatedTests",
   ],
-  "src-tauri/**/*": ["cargo test"],
+  "src-tauri/**/*": [
+    pipe(
+      partition(test(/\.rs$/)),
+      ([rsFiles, nonRsFiles]) =>
+        `cd src-tauri && ${
+          nonRsFiles.length
+            ? // run all test tests
+              "cargo test"
+            : // run specific files only
+              `cargo test ${rsFiles.join(",")}`
+        }`
+    ),
+  ],
 };
