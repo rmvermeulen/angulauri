b3950e2873ba7e495e98baf2d385869abc011ee1
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-18 18:05:14 +0100

    Put the page of items in an *ngIf so it only shows when it exists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,50 +2,31 @@ import { Component } from '@angular/core'
 import { PageEvent } from '@angular/material/paginator'
 import { BehaviorSubject, combineLatest, EMPTY, from, Observable } from 'rxjs'
 import { debounceTime, filter, map, switchMap } from 'rxjs/operators'
-import { TauriService } from './tauri.service'
+import { GetItemsResponse, TauriService } from './tauri.service'
 
 @Component({
   selector: 'app-root',
-  styles: [
-    `
-      div * {
-        display: inline-block;
-      }
-    `,
-  ],
+  styles: [],
   template: `
-    <span id="title">{{ title }} app is running!</span>
-    <p>resourceId: {{ resourceId$ | async }}</p>
-    <p>items: {{ items$ | async }}</p>
-    <p>has-prev: {{ hasPrevPage$ | async }}</p>
-    <p>has-next: {{ hasNextPage$ | async }}</p>
-
-    <p>router:</p>
     <router-outlet></router-outlet>
-    <p>\\router</p>
+    <span id="title">{{ title }} app is running!</span>
+    <div *ngIf="itemPage$ | async as page">
+      <p>resourceId: {{ resourceId$ | async }}</p>
+      <p>items: {{ page.items }}</p>
+      <p>has-prev: {{ page.hasPrev }}</p>
+      <p>has-next: {{ page.hasNext }}</p>
 
-    <mat-paginator
-      [disabled]="false"
-      [pageIndex]="page$ | async"
-      [pageSize]="pageSize$ | async"
-      [length]="100"
-      [pageSizeOptions]="[5, 10, 15, 25, 50]"
-      (page)="handlePageEvent($event)"
-    ></mat-paginator>
+      <p>router:</p>
+      <p>\\router</p>
 
-    <div>
-      <button (click)="decPage()" [disabled]="(hasPrevPage$ | async) === false">
-        &lt;
-      </button>
-      <p>{{ page$ | async }}</p>
-      <button (click)="incPage()" [disabled]="(hasNextPage$ | async) === false">
-        &gt;
-      </button>
-    </div>
-    <div>
-      <button (click)="decPageSize()">&lt;</button>
-      <p>{{ pageSize$ | async }}</p>
-      <button (click)="incPageSize()">&gt;</button>
+      <mat-paginator
+        [disabled]="false"
+        [pageIndex]="page$ | async"
+        [pageSize]="pageSize$ | async"
+        [length]="100"
+        [pageSizeOptions]="[5, 10, 15, 25, 50]"
+        (page)="handlePageEvent($event)"
+      ></mat-paginator>
     </div>
     <app-resource (resource)="loadResource($event)"></app-resource>
     <router-outlet></router-outlet>
@@ -56,40 +37,19 @@ export class AppComponent {
   resourceId$ = new BehaviorSubject('')
   page$ = new BehaviorSubject(0)
   pageSize$ = new BehaviorSubject(10)
-  response$: Observable<any>
-  items$: Observable<string[]>
-  hasNextPage$: Observable<boolean>
-  hasPrevPage$: Observable<boolean>
+  itemPage$: Observable<GetItemsResponse<string> & { id: string }>
 
   constructor(private readonly tauri: TauriService) {
     const validId$ = this.resourceId$.pipe(filter((id) => id.length > 0))
-    this.response$ = combineLatest([this.page$, this.pageSize$, validId$]).pipe(
+    this.itemPage$ = combineLatest([this.page$, this.pageSize$, validId$]).pipe(
       debounceTime(250),
       switchMap(([page, pageSize, id]: [number, number, string]) =>
-        from(this.tauri.getItems<string>(id, page, pageSize)),
+        from(this.tauri.getItems<string>(id, page, pageSize)).pipe(
+          map((res) => ({ id, ...res })),
+        ),
       ),
     )
-    this.items$ = this.response$.pipe(map(({ items }) => items))
-    this.hasNextPage$ = this.response$.pipe(map(({ hasNext }) => hasNext))
-    this.hasPrevPage$ = this.response$.pipe(map(({ hasPrev }) => hasPrev))
   }
-
-  incPage() {
-    this.page$.next(this.page$.value + 1)
-  }
-
-  decPage() {
-    this.page$.next(Math.max(0, this.page$.value - 1))
-  }
-
-  incPageSize() {
-    this.pageSize$.next(this.pageSize$.value + 1)
-  }
-
-  decPageSize() {
-    this.pageSize$.next(Math.max(1, this.pageSize$.value - 1))
-  }
-
   loadResource(id: string) {
     this.resourceId$.next(id)
   }
