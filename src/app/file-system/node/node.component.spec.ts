f0cddd505e42e8b7719ce1ec137eca235dac8da8
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-04-18 22:17:25 +0200

    Update test and snapshots

--- src/app/file-system/node/node.component.spec.ts
+++ src/app/file-system/node/node.component.spec.ts
@@ -1,64 +1,99 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { INodeData, NodeComponent } from './node.component'
+import { INode, NodeComponent } from './node.component'
 
-describe.each([
-  undefined,
-  {
-    path: '/',
-    content: 'a file',
-  },
-  {
-    path: '/',
-    children: [
-      {
-        path: 'icon.png',
-        content: 'a file',
-      },
-      {
-        path: 'icon.jpg',
-        content: 'a file',
-      },
-    ],
-  },
-])('NodeComponent data=%p', (data: INodeData | undefined) => {
+describe('NodeComponent (empty)', () => {
   let component: NodeComponent
   let fixture: ComponentFixture<NodeComponent>
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NodeComponent],
     }).compileComponents()
   })
 
-  beforeAll(() => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(NodeComponent)
     component = fixture.componentInstance
-    component.data = data
-    fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
-    expect(fixture.nativeElement).toMatchSnapshot()
-    expect(fixture.nativeElement.querySelector('div')).toMatchSnapshot()
   })
 
-  it('matches expected output', () => {
-    expect(fixture).toMatchSnapshot()
+  it('should render with no data', () => {
+    expect(fixture.nativeElement).toMatchInlineSnapshot(`
+      <div
+        id="root1"
+      >
+        
+        
+      </div>
+    `)
   })
 
-  it.skip('should render a .node', () => {
-    const node = fixture.nativeElement.querySelector('.node')
-    expect(node).toBeTruthy()
+  it('should render with simple file', () => {
+    component.data = {
+      path: '/',
+      content: 'a file',
+    }
+    fixture.detectChanges()
+    expect(fixture.nativeElement).toMatchInlineSnapshot(`
+      <div
+        id="root2"
+      >
+        
+        <div
+          class="node odd file"
+        >
+          
+          <div>
+            <p>
+               file 
+              <code>
+                /
+              </code>
+               contains: 
+              <code>
+                a file
+              </code>
+            </p>
+          </div>
+          
+        </div>
+        
+      </div>
+    `)
   })
 
-  it.skip('should render a .file', () => {
-    const file = fixture.nativeElement.querySelector('.file')
-    expect(file).toBeTruthy()
-  })
-
-  it.skip('should render a .folder', () => {
-    const folder = fixture.nativeElement.querySelector('.folder')
-    expect(folder).toBeTruthy()
+  it('should render with empty folder', () => {
+    component.data = {
+      path: '/',
+      children: [],
+    }
+    fixture.detectChanges()
+    expect(fixture.nativeElement).toMatchInlineSnapshot(`
+      <div
+        id="root3"
+      >
+        
+        <div
+          class="node odd folder"
+        >
+          
+          
+          <div>
+            <p>
+               folder 
+              <code>
+                /
+              </code>
+               contains: 
+            </p>
+            
+          </div>
+        </div>
+        
+      </div>
+    `)
   })
 })
